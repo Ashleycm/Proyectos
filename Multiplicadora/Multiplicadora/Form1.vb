c02d42660b9c741f916e3841f59947ecda482e40
Ashleycm <sinhxetos@gmail.com>
2019-10-14 21:08:34 +0200

    Multiplicadora

---
+++ Multiplicadora/Multiplicadora/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+
+    Dim n1 As Integer
+    Dim n2 As Integer
+    Dim salida As String
+
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        n1 = TextBox1.Text
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        n2 = TextBox2.Text
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim resultado As Integer
+        n1 = TextBox1.Text
+        n2 = TextBox2.Text
+        For i As Integer = 0 To n2
+            resultado = n1 * i
+            salida = n1 & "x" & i & "=" & resultado
+            TextBox3.Text = TextBox3.Text + salida + vbNewLine
+
+
+
+
+
+        Next
+
+
+
+
+    End Sub
+End Class
